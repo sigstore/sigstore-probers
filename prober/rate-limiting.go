83f3206dfc4c8680a547cd58891e32fc64171b6c
Hayden Blauzvern <hblauzvern@google.com>
2023-01-19 23:22:15 +0000

    Fix prober segfault
    When a connection error occurred, the prober printed the error, but
    continued to try to get the status code of a nil object. This change
    logs an error (now to stderr) but does not continue. We now count the
    number of failures (which should be zero, but we are seeing Fulcio
    returning failures frequently).
    
    Also removed some of the other logging unless there was an unexpected
    value.
    
    Signed-off-by: Hayden Blauzvern <hblauzvern@google.com>

--- prober/rate-limiting.go
+++ prober/rate-limiting.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"sync/atomic"
 )
 
 var (
@@ -32,6 +33,7 @@ func rateLimit() error {
 	if url == "" {
 		return fmt.Errorf("please set url to rate limit via --url flag")
 	}
+	var errCount uint64
 	wg := sync.WaitGroup{}
 	rateLimited := false
 	// Currently 975 req/min is allowed, or 16/s
@@ -43,21 +45,24 @@ func rateLimit() error {
 		go func() {
 			req, err := http.Get(url)
 			if err != nil {
-				fmt.Println(err)
-			}
-			// We want a 429 status code to show that rate limiting worked
-			if req.StatusCode == 429 {
+				fmt.Fprintln(os.Stderr, err)
+				atomic.AddUint64(&errCount, 1)
+			} else if req.StatusCode == 429 {
+				// We want a 429 status code to show that rate limiting worked
 				b, _ := ioutil.ReadAll(req.Body)
 				if strings.Contains(string(b), "Too Many Requests") {
 					rateLimited = true
-					fmt.Println("Received 429 status code, rate limiting successful.")
+				} else {
+					fmt.Fprintln(os.Stderr, string(b))
 				}
-				fmt.Println(string(b))
 			}
 			wg.Done()
 		}()
 	}
 	wg.Wait()
+	if errCount > 0 {
+		fmt.Printf("%d out of %d requests had connection errors\n", errCount, limit)
+	}
 	if !rateLimited {
 		return fmt.Errorf("No 429 status code was received, rate limiting may not have worked")
 	}
